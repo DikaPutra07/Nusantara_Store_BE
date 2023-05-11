d8835eb9c03d7ad08658b7f0d745c2127aa7cf66
puyul123 <andikalaksana.putra@gmail.com>
2023-05-11 18:20:37 +0700

    minor-update

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const db_name = process.env.DB_NAME
 const db_user = process.env.DB_USER
-const db_pass = process.env.DB_PASS
+const db_pass = process.env.DB_PASSWORD
 const db_host = process.env.DB_HOST
 const db_port = process.env.DB_PORT
 
